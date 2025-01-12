194367ef6e45c9b1d974691c134917ddc7b3d708
serkkaddis <serkaddistadesseb@gmail.com>
2025-01-12 22:05:46 +0300

    Remove exposed API key from firebaseConfig.jsx

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCKp9JUf998vQyVddlCRHiu2auBpIc-m-U",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "travel-planner-4e495.firebaseapp.com",
   projectId: "travel-planner-4e495",
   storageBucket: "travel-planner-4e495.firebasestorage.app",
